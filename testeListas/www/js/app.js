ac7177439434e94ef7616d80a458a45d4b1aec94
denilson_jpf <denilson_jpf@yahoo.com.br>
2020-04-07 18:40:52 -0300

    add project

---
+++ testeListas/www/js/app.js
@@ -0,0 +1,39 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.Keyboard) {
+      window.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller("MeuController", function($scope){
+
+  $scope.itens = ["UVA", "MORANGO", "BANANA", "PERA"];
+  
+  // definir uma função anonima para moveItem que irá alterar a posição dos itens
+  $scope.moveItem = function(item, fromIndex, toIndex){
+    //splice nos ajuda a capturar a posição incial do item
+    $scope.itens.splice(fromIndex, 1); 
+    //vamos definir a nova posição do item na lista
+    $scope.itens.splice(toIndex, 0, item);
+  };
+
+})
